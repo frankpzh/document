88c58ab9eba0c8fafc476e6fa9b6bcc53d1b6b5a
Frank Pan <frankpzh@gmail.com>
2010-09-19 15:53:48 +0800

    laptop change

--- lisp/utility.el
+++ lisp/utility.el
@@ -1,25 +1,33 @@
 (defvar select-tag-list
-  (list
-   (cons ?x "~/xen/xen")
-   (cons ?q "~/xen/tools/ioemu-remote")
-   (cons ?k "/usr/src/linux")
-   (cons ?i "~/igb-mig")
-   (cons ?c "~/xen/tools/libxc")
-   (cons ?p "~/xen/tools/python")))
+  ((?x "~/xen/xen")
+   (?q "~/xen/tools/ioemu-remote")
+   (?k "/usr/src/linux")
+   (?i "~/igb-mig")
+   (?c "~/xen/tools/libxc")
+   (?p "~/xen/tools/python")))
 
 (defun select-tag (char)
   (interactive "cTag code:")
   (let ((tag
-	 (catch 'result
-	   (let ((list select-tag-list))
-	     (while (not (null list))
-	       (if (equal char (caar list))
-		   (throw 'result (cdar list)))
-	       (setq list (cdr list))))
-	   nil)))
+         (catch 'result
+           (let ((list select-tag-list))
+             (while (not (null list))
+               (if (equal char (caar list))
+                   (throw 'result (cdar list)))
+               (setq list (cdr list))))
+           nil)))
     (if (null tag)
-	(message "No such tag code.")
-      (setq tags-file-name tag))))
+        (message "No such tag code.")
+      (progn
+        (setq tags-table-list nil)
+        (setq tags-file-name tag)
+        (message (concat "Tag file set to " tag))))))
+
+(defun clear-tag-list nil
+  (interactive)
+  (setq tags-file-name nil)
+  (setq tags-table-list nil)
+  (message "Tag file list has been cleared"))
 
 (defun word-count nil
   (interactive)
