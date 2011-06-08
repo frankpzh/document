b1730137efd3ba5e33a8888b1f2a1369795ae8bb
Frank Pan <frankpzh@gmail.com>
2011-06-08 23:25:42 +0800

    change of maximize-emacs

--- lisp/maximize-emacs.el
+++ lisp/maximize-emacs.el
@@ -5,10 +5,20 @@
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                          '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
 
+(defun maximize-emacs nil
+  (interactive)
+  (if (eq system-type 'windows-nt)
+      (w32-send-sys-command #xf030)
+    (maximize-emacs-unix)))
+
+(defun fullscreen nil
+  (interactive)
+  (set-frame-parameter nil 'fullscreen
+                       (if (frame-parameter nil 'fullscreen)
+                           nil
+                         'fullboth)))
+
 (add-hook 'window-setup-hook
-		  '(lambda nil
-			 (if (eq system-type 'windows-nt)
-				 (w32-send-sys-command #xf030)
-			   (maximize-emacs-unix))) t)
+          'maximize-emacs t)
 
 (provide 'maximize-emacs)
