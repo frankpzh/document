96e82ed65a402ef421f88b636ce9c0d2fe9def1b
Frank Pan <frankpzh@gmail.com>
2011-08-10 11:16:55 +0800

    org2tex script change

--- scripts/org2tex.el
+++ scripts/org2tex.el
@@ -1,6 +1,17 @@
 #!/usr/bin/emacs --script
 
-(require 'org-export-latex)
+(condition-case nil
+    (progn
+      ; These are for Orgmode 7
+      (require 'org-latex)
+      (require 'org-special-blocks)
+      (setq org-export-latex-default-packages-alist
+            '(("" "hyperref" nil)
+              ("" "graphicx" t))))
+  (error
+   ; Fall back to Orgmode 6
+   (require 'org-export-latex)))
+
 (add-to-list 'org-export-latex-classes
              '("usenix"
                "\\documentclass[finalversion,endnotes]{usetex-v1}
@@ -11,9 +22,16 @@
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                ("\\paragraph{%s}" . "\\paragraph*{%s}")
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(add-to-list 'org-export-latex-classes
+             '("ieee"
+               "\\documentclass[conference]{IEEEtran}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")))
 (add-to-list 'org-export-latex-inline-image-extensions
              "eps")
 
 (setq org-file (pop argv))
 (find-file org-file)
-(org-export-as-latex 3)
+(org-export-as-latex 5)
